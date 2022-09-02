ea17433bc1e535dbe20b809c0fdece5e478a2441
tomazpozrl <tomaz.pozrl@fs.uni-lj.si>
2022-09-02 16:42:20 +0200

    car commands and HTTP request move tested

--- robot_car_control/robot_car_control.ino
+++ robot_car_control/robot_car_control.ino
@@ -7,33 +7,38 @@
 #include <HTTPClient.h>
 
 // **************** global app variables *********************
-int state = 0; // default: 0
-bool busy = true; // default: false
-int sensorGrayscaleSplittingValue = 150;
+int state = 1; // default: 1
+bool busy = false; // default: false
 // variables for robot cars' infrared sensors (1 means black, 0 means white)
 int L1, L2, L3, R1, R2, R3;
+// this flag is set to true in the first iteration of the loop after busy variable is set to true and set to false immediately after that
+int flag = false;
 
 // start and end location for the robot car moves
 //    these two values are set when the request to move is received via WiFi
 //    NOTE: the request to move is only processed after the previous request (move) is finished --> this is controlled by the outside app
-long sourceLocation = 5; // default: 0
-long targetLocation = 1; // default: 0
+long sourceLocation = 0; // default: 0
+long targetLocation = 0; // default: 0
 long taskId = 1; // default: 0
 
 // production areas locations: 1, 2, 3, 4
 int firstProductionAreaLocation = 1;
-int lastProductionAreaLocation = 4;
+int lastProductionAreaLocation = 1;
 int mainRoboticArmLocation = 5;
 // parking areas locations: 6, 7
 int firstParkingAreaLocation = 6;
-int lastParkingAreaLocation = 7;
+int lastParkingAreaLocation = 6;
 
 int numOfRightTurnsToPass, numOfLeftTurnsToPass;
 int numOfRightTurns = 0, numOfLeftTurns = 0;
 
 // **************** WiFi parameters *********************
-const char* ssid     = "DESKTOP-4NT77NL-4917";
+const char* ssid     = "DESKTOP-JL5IS04 3781";
 const char* password = "TPlaptopHOTSPOT";
+
+//const char* ssid     = "DESKTOP-4NT77NL-4917";
+//const char* password = "TPlaptopHOTSPOT";
+
 WebServer server(80);
 
 // **************** Mbits I2C settings *********************
@@ -127,11 +132,11 @@ void ShowString(String sMessage, CRGB myRGBcolor) {
         }
       }
       FastLED.show();
-      if (sft < sensorGrayscaleSplittingValue) {
-        FastLED.delay(200);
-      } else {
-        FastLED.delay(30);
-      }
+      //      if (sft < sensorGrayscaleSplittingValue) {
+      //        FastLED.delay(200);
+      //      } else {
+      //        FastLED.delay(30);
+      //      }
     }
   }
 }
@@ -163,7 +168,21 @@ void handleMove() {
       sourceLocation = server.arg("source").toInt();
       targetLocation = server.arg("target").toInt();
       taskId = server.arg("taskId").toInt();
-      // busy = true;
+      busy = true;
+      flag = true;
+      // if the target is one of the robotic arms (warehouse or production)
+      if (targetLocation <= mainRoboticArmLocation) {
+        numOfRightTurnsToPass = getRightTurnsToPass();
+        numOfLeftTurnsToPass = 0;
+      }
+      // if the target is a parking area
+      else {
+        numOfLeftTurnsToPass = getLeftTurnsToPass();
+        numOfRightTurnsToPass = 0;
+      }
+      numOfLeftTurns = 0;
+      numOfRightTurns = 0;
+
       Serial.println("source: " + String(sourceLocation) + ", target: " + String(targetLocation) + ", taskId: " + String(taskId));
       server.send(200, "text/plain", "{status:accept}");
     }
@@ -232,7 +251,7 @@ void setup() {
   // set MaqueenPlus front lights color
   mp->setRGB(mp->eALL, mp->eBLUE);
   // enable MaqueenPlus PID operation control
-  mp->PIDSwitch(mp->eON);
+  mp->PIDSwitch(mp->eOFF);
 }
 
 void loop() {
@@ -263,38 +282,37 @@ void loop() {
   // car robot driving algorithm
   switch (state) {
     // default case, the car is free (busy = false) and waits for the next request to move
-    case 0:
+    case 1:
       if (busy) {
         // start the move
+        moveForward();
 
         //the car detects the location for the left or right turn (coming out of the parking or production area)
         // the car turns left if it is coming out of parking area or it turns right if it is coming out of production area
         if (L1 && R1 && !L3 && !R3) {
-          //    stop();
+          stop();
           // car is coming out of parking area --> it turns left
           if (sourceLocation > targetLocation) {
+            Serial.println("car is coming out of parking area --> turn left");
             state = 2;
           }
           // car is coming of production area --> it turns right
           else {
+            Serial.println("car is coming of production area --> turn right");
             state = 4;
           }
           break;
         }
-        // the car detects the end location for the car move (either the production area or the parking area)
-        else if (!L3 && !R3) {
-          stop();
-          state = 8;
-          break;
-        }
         // the car detects the location for the left turn at the grid outer borders -- > the car must turn left
-        else if (!L3) {
+        else if (L1 && R1 & !L3 && R3) {
+          Serial.println("the car detects the location for the left turn at the grid outer borders -- > the car must turn left");
           stop();
           state = 2;
           break;
         }
         //the car detects the location for the right turn at the grid outer borders --> the car must turn right
-        else if (!R3) {
+        else if (L1 && R1 && L3 && !R3) {
+          Serial.println("the car detects the location for the right turn at the grid outer borders --> the car must turn right");
           stop();
           state = 4;
           break;
@@ -303,37 +321,49 @@ void loop() {
         // 1. it is moving from a production area (1 to 4) to a parking area (5 to 9)
         // 2. it has passed the "numOfLeftTurnsToPass" turns
         else if ((!L1 || !R1) && !L3 && R3) {
-          if ((sourceLocation < targetLocation) && (numOfLeftTurns >= numOfLeftTurnsToPass)) {
+          //          if ((sourceLocation < targetLocation) && (numOfLeftTurns >= numOfLeftTurnsToPass)) {
+          if (sourceLocation < targetLocation) {
             stop();
             state = 3;
             break;
           }
-          else {
-            numOfLeftTurns++;
-            break;
-          }
+          //          else {
+          //            numOfLeftTurns++;
+          //            break;
+          //          }
         }
         //the car detects the location for the right turn at the production area --> the car turns right under two conditions:
         // 1. it is moving from a parking area (5 to 9) to a production area (1 to 4)
         // 2. it has passed the "numOfRightRightTurnsToPass" turns
         else if ((!L1 || !R1) && L3 && !R3) {
-          if ((sourceLocation > targetLocation) && (numOfRightTurns >= numOfRightTurnsToPass)) {
+          Serial.println("condition met for right turn to the production area");
+          //          if ((sourceLocation > targetLocation) && (numOfRightTurns >= numOfRightTurnsToPass)) {
+          if (sourceLocation > targetLocation) {
             stop();
             state = 5;
             break;
           }
-          else {
-            numOfRightTurns++;
-            break;
-          }
+          //          else {
+          //            numOfRightTurns++;
+          //            break;
+          //          }
         }
-        else {
-          // move forward
-          state = 0;
+        else if ((!L1 || !R1) && L3 && R3) {
+          // no condition met for turning left or right, continue to move forward
+          Serial.println("no condition met for turning left or right, continue to move forward");
+          state = 1;
         }
-        break;
+        // the car detects the end location for the car move (either the production area or the parking area)
+        else if ((!L1 || !R1) && !L3 && !R3) {
+          Serial.println("the car detects the end location for the car move");
+          stop();
+          state = 6;
+          break;
+        }
       }
-
+      else {
+        Serial.println("no request to move received");
+      }
       break;
 
     case 555:
@@ -342,59 +372,60 @@ void loop() {
       moveForwardFix();
       break;
 
-    // case 1: the car moves forward following the line
-    case 1:
+    // case 111: the car moves forward following the line
+    case 111:
       moveForward();
+      state = 1;
+      break;
 
     // case 2: turn the car left (at the grid outer turns or coming out of the parking area)
     case 2:
       turnLeft();
-      // the car turns until the sensors L1 and R1 detect the line
-      if (!L1 && !R1) {
+      // the car turns until the sensors R2 detects the line
+      if (!R1) {
         stop();
-        state = 0;
+        state = 1;
       }
       break;
 
     // case 3: turn the car left (going to the parking area)
     case 3:
       turnLeft();
-      // the car turns until the sensors L3 and R3 and either L1 or R1 detect the line
-      if ((!L1 || !R1) && !L3 && !R3) {
+      // the car turns until the sensor R3 detects the line
+      if (!R3) {
         stop();
-        state = 0;
+        state = 1;
       }
       break;
 
-    // case 4: turn the car right (at the grid outer turns coming out of production area)
+    // case 4: turn the car right (at the grid outer turns or coming out of the production area)
     case 4:
       turnRight();
-      // the car turns until the sensors L1 and R1 detect the line
-      if (!L1 && !R1) {
+      // the car turns until the sensors L2 detects the line
+      if (!L1) {
         stop();
-        state = 0;
+        state = 1;
       }
       break;
 
     // case 5: turn the car right (going to the production area)
     case 5:
       turnRight();
-      // the car turns until the sensors L3 and R3 and either L1 or R1 detect the line
-      if ((!L1 || !R1) && !L3 && !R3) {
+      // the car turns until the sensors L3
+      if (!L3) {
         stop();
-        state = 0;
+        state = 1;
       }
       break;
-
-    // case 6: the car is at the end location --> send http message to Node.js control app and switch to state 0
+    // case 6: the car is at the end location --> send http message to Node.js control app and switch to state 1
     case 6:
-      state = 0;
-      break;
+      busy = false;
+      state = 1;
 
-      //      // call Node.js control app
-      //      // create HTTP client
-      //      HTTPClient http;
-      //      // set server host and path
+      // call Node.js control app
+      // create HTTP client
+      HTTPClient http;
+      // set server host and path
       //      http.begin("193.2.80.85/report?taskId" + String(taskId) + "&state=done"); //HTTP
       //      // start the connection and send HTTP header
       //      int httpCode = http.GET();
@@ -415,6 +446,7 @@ void loop() {
       //        Serial.println("HTTP GET failed, error:");
       //        Serial.println(http.errorToString(httpCode).c_str());
       //      }
+      break;
   }
 }
 
@@ -424,60 +456,83 @@ void moveForwardFix() {
 
 // basic line following
 void moveForward() {
-  // Serial.println("moving forward");
+  Serial.println("moving forward");
+
   // 1001
   if (L2 && !L1 && !R1 && R2) {
     mp->motorControl(mp->eALL, mp->eCW, 50);
-    //    ShowChar('A', myRGBcolor_zyvx);
+    //    Serial.println("A");
   }
   // 1101
   else if (L2 && L1 && !R1 && R2) {
     mp->motorControl(mp->eLEFT, mp->eCW, 50);
     mp->motorControl(mp->eRIGHT, mp->eCW, 37);
+    //    Serial.println("B");
   }
   // 1100
   else if (L2 && L1 && !R1 && !R2) {
     mp->motorControl(mp->eLEFT, mp->eCW, 50);
     mp->motorControl(mp->eRIGHT, mp->eCW, 0);
+    //    Serial.println("C");
   }
   // 1110
   else if (L2 && L1 && R1 && !R2) {
     mp->motorControl(mp->eLEFT, mp->eCW, 50);
     mp->motorControl(mp->eRIGHT, mp->eCCW, 20);
+    //    Serial.println("D");
   }
   // 1011
   else if (L2 && !L1 && R1 && R2) {
     mp->motorControl(mp->eLEFT, mp->eCW, 37);
     mp->motorControl(mp->eRIGHT, mp->eCW, 50);
+    //    Serial.println("E");
   }
   // 0011
   else if (!L2 && !L1 && R1 && R2) {
     mp->motorControl(mp->eLEFT, mp->eCW, 0);
     mp->motorControl(mp->eRIGHT, mp->eCW, 50);
+    //    Serial.println("F");
   }
+
   // 0111
   else if (!L2 && L1 && R1 && R2) {
     mp->motorControl(mp->eLEFT, mp->eCCW, 20);
     mp->motorControl(mp->eRIGHT, mp->eCW, 50);
+    //    Serial.println("G");
   }
   // 1111
   else if (L2 && L1 && R1 && R2) {
     mp->motorControl(mp->eALL, mp->eCW, 50);
+    //    Serial.println("H");
+  }
+
+  // if the flag is true (during the first iteration of the loop function after busy is set to true, wait for 500 ms
+  // this is to ensure that the car moves from the original position (current parking or productiona area)
+  if (flag) {
+    delay(500);
+    flag = false;
+    // read the sensor values again otherwise the car will stop
+    L1 = mp->getPatrol(mp->eL1);
+    L2 = mp->getPatrol(mp->eL2);
+    L3 = mp->getPatrol(mp->eL3);
+    R1 = mp->getPatrol(mp->eR1);
+    R2 = mp->getPatrol(mp->eR2);
+    R3 = mp->getPatrol(mp->eR3);
   }
 }
 
 // turn left: move the right motor backwards (counter clockwise) and the left motor forward (clockwise)
 void turnLeft() {
-  // Serial.println("turning left");
-  mp->motorControl(mp->eLEFT, mp->eCCW, 37);
-  mp->motorControl(mp->eRIGHT, mp->eCW, 37);
+  Serial.println("turning left");
+  mp->motorControl(mp->eLEFT, mp->eCCW, 45);
+  mp->motorControl(mp->eRIGHT, mp->eCW, 45);
 }
 
 // turn right: move the left motor backwards (counter clockwise) and the right motor forward (clockwise)
 void turnRight() {
-  // Serial.println("turning right");
-  mp->motorControl(mp->eLEFT, mp->eCW, 37);
-  mp->motorControl(mp->eRIGHT, mp->eCCW, 37);
+  Serial.println("turning right");
+  mp->motorControl(mp->eLEFT, mp->eCW, 45);
+  mp->motorControl(mp->eRIGHT, mp->eCCW, 45);
 }
 
 // stop: stop both motors
@@ -487,22 +542,60 @@ void stop() {
   mp->motorControl(mp->eALL, mp->eCCW, 0);
 }
 
+// calculate how many left turn to pass if going to a parking area
+// this depends on the source location and the id of the parking area
+int getLeftTurnsToPass() {
+  int leftTurnsToPass = 0;
+  // if the source is one of the production areas
+  if (sourceLocation < mainRoboticArmLocation) {
+    leftTurnsToPass = 2 * (targetLocation - firstParkingAreaLocation);
+  }
+  // if the source is the warehouse
+  else {
+    // if the target is positioned before the warehouse (anti-clockwise)
+    if ((targetLocation - sourceLocation) <= 2) {
+      leftTurnsToPass = 2 * 2 + 2 * (targetLocation - firstParkingAreaLocation);
+    }
+    // if the target is positioned behind the warehouse (anti-clockwise) and is the last parking area, we skip the one before it
+    else if (targetLocation == lastParkingAreaLocation) {
+      leftTurnsToPass = 2;
+    }
+    // else: the target is the first parking after the warehouse and there are no left turns to pass
+  }
+  return leftTurnsToPass;
+}
+
+// calculate how many right turn to pass if going to a parking area
+// this depends on the source location and the id of the parking area
+int getRightTurnsToPass() {
+  int rightTurnsToPass = 0;
+  // if we move from a warehouse to a production area
+  if (sourceLocation > targetLocation ) {
+    rightTurnsToPass = 2 * (targetLocation - firstProductionAreaLocation);
+  }
+  // if we move from a production area to the warehouse
+  else {
+    rightTurnsToPass = 2 * (lastProductionAreaLocation - sourceLocation);
+  }
+  return rightTurnsToPass;
+}
+
 // check current values of all relevant sensors
 void checkAllSensors() {
 
-//  unsigned long time1 = micros();
-//  Serial.println("L1: " + String(mp->getPatrol(mp->eL1)));
-//  unsigned long time2 = micros();
-//  Serial.println("L2: " + String(mp->getPatrol(mp->eL2)));
-//  unsigned long time3 = micros();
-//  Serial.println("L3: " + String(mp->getPatrol(mp->eL3)));
-//  unsigned long time4 = micros();
-//  Serial.println("R1: " + String(mp->getPatrol(mp->eR1)));
-//  unsigned long time5 = micros();
-//  Serial.println("R2: " + String(mp->getPatrol(mp->eR2)));
-//  unsigned long time6 = micros();
-//  Serial.println("R3: " + String(mp->getPatrol(mp->eR3)));
-//  unsigned long time7 = micros();
+  //  unsigned long time1 = micros();
+  //  Serial.println("L1: " + String(mp->getPatrol(mp->eL1)));
+  //  unsigned long time2 = micros();
+  //  Serial.println("L2: " + String(mp->getPatrol(mp->eL2)));
+  //  unsigned long time3 = micros();
+  //  Serial.println("L3: " + String(mp->getPatrol(mp->eL3)));
+  //  unsigned long time4 = micros();
+  //  Serial.println("R1: " + String(mp->getPatrol(mp->eR1)));
+  //  unsigned long time5 = micros();
+  //  Serial.println("R2: " + String(mp->getPatrol(mp->eR2)));
+  //  unsigned long time6 = micros();
+  //  Serial.println("R3: " + String(mp->getPatrol(mp->eR3)));
+  //  unsigned long time7 = micros();
 
   // double duration1 = (double)(time2 - time1);
   // double duration2 = (double)(time3 - time2);
