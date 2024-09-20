56b0f2bb6c6fc382121522d44786906511f02406
Tomaž Požrl <87708323+tomazpozrl@users.noreply.github.com>
2024-09-20 12:59:29 +0200

    Update config.cpp

--- robot_car_control_core/config.cpp
+++ robot_car_control_core/config.cpp
@@ -4,7 +4,7 @@
 const char* ssid = "NSSM";        // Define network ssid
 const char* password = "NSSM1234"; // Define password
 
-const char* studentAppIP = "http://192.168.1.100";  // IP address of student app
+const char* studentAppIP = "http://192.168.137.200";  // IP address of the student app
 const char* endpoint = "/receiveUID";  // endpoint for sending detected RFID IDs
 
 
