a6b973138ca2f9f2405a3001167e74cec5bda809
tomazpozrl <tomaz.pozrl@fs.uni-lj.si>
2025-09-19 11:20:05 +0200

    Update config.cpp

--- robot_car_control_core/config.cpp
+++ robot_car_control_core/config.cpp
@@ -1,10 +1,8 @@
 // config.cpp
 #include "config.h"
 
-const char* ssid = "NSSM";        // Define network ssid
-const char* password = "NSSM1234"; // Define password
+const char* ssid = "Tenda_C2E100";        // Define network ssid
+const char* password = "12345678"; // Define password
 
 char studentAppIP[16] = "192.168.137.200";  // IP address of the student app
 const char* endpoint = "/receiveUID";  // endpoint for sending detected RFID IDs
-
-
